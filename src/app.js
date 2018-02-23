295cac1e067c5669cade970ce84b655b72c9ed3b
George Higbie <gehigbie@gmail.com>
2018-02-23 22:13:12 +0000

    Add basic structure to app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-DOM';
+import ReactDOM from 'react-dom';
+
+import ExpensifyApp from './component/ExpensifyApp'
 import './styles/styles.css';
 
+
+ReactDom.render(ExpensifyApp, document.getElementById('react-container'));
